44a8ca54e82ca07f0219fa71de08096616463e0b
Chris I Powell <chris@chrisipowell.co.uk>
2016-10-29 23:46:15 +0100

    fix test creds

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,6 @@ config :chrisipowell_api, ChrisipowellApi.Repo,
   adapter: Ecto.Adapters.MySQL,
   database: "chrisipowell_api_repo",
   username: "root",
-  password: "password123",
+  password: "",
   hostname: "127.0.0.1",
-  port: 32769,
   pool: Ecto.Adapters.SQL.Sandbox
