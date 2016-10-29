e19234f6a321ae4cbdfd9e96b89a43984f375624
Chris I Powell <chris@chrisipowell.co.uk>
2016-10-29 21:54:53 +0100

    update deps

--- mix.exs
+++ mix.exs
@@ -30,7 +30,7 @@ defmodule ChrisipowellApi.Mixfile do
   defp deps do
     [{:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~3.0"},
+     {:phoenix_ecto, "~> 3.0"},
      {:mariaex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
      {:cowboy, "~> 1.0"},
