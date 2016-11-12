bf6c65211e4752699d6df4ef6e6076f4aa07fafa
Chris I Powell <chris@chrisipowell.co.uk>
2016-11-12 14:23:12 +0000

    fix rest properly

--- test/controllers/app_controller_test.exs
+++ test/controllers/app_controller_test.exs
@@ -2,7 +2,7 @@ defmodule ChrisipowellApi.AppControllerTest do
   use ChrisipowellApi.ConnCase, async: true
 
   test "index/2 reponds with Hello world" do
-    response = get(build_conn(), "/api")
+    response = get(build_conn(), "/")
        |> json_response(200)
 
     expected = %{ "application" => "chrisipowell_api", "version" => "0.2.0" }
